ba956213806bd69e27bd4e9e7f13ffdcde571076
gns0304 <hjhhan1012!>
2020-09-18 00:35:04 +0900

    clean commit

--- Mieum/Mieum/wsgi.py
+++ Mieum/Mieum/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Mieumproject project.
+WSGI config for Mieum project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mieumproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mieum.settings')
 
 application = get_wsgi_application()
